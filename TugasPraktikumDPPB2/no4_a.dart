abf8cbdcbd7b51dd2e55d604efaad36633363b38
Ahmad Ammar Salas <ammarsalas27@gmail.com>
2025-10-04 23:34:40 +0700

    Delete TugasPraktikumDPPB2 directory

--- TugasPraktikumDPPB2/no4_a.dart
+++
@@ -1,8 +0,0 @@
-void main(){
-  var list1 = [1, 2, 3];
-  var list2 = [0, ...list1];
-
-  print(list1);
-  print(list2);
-  print(list2.length);
-}
--- TugasPraktikumDPPB2/no5_a.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:collection';
-
-void main() {
-  Queue<String> antrean = Queue<String>();
-  antrean.addAll(["Budi", "Ani", "Citra"]);
-
-  print("Daftar antrean awal:");
-  print(antrean);
-}
